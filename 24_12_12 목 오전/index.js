3d39cf704377cc72dcd10b83207ab7db98f1d471
oyhghs123 <oyhghs123@naver.com>
2024-12-12 17:29:37 +0900

    second commit

---
+++ 24_12_12 목 오전/index.js
@@ -0,0 +1,11 @@
+// 실행하고 싶은 동작
+function show() {
+  const modal = document.getElementById("modal");
+  modal.style = "display:flex";
+}
+
+// 끄는 동작
+function hide() {
+  const modal = document.getElementById("modal");
+  modal.style = "display:none";
+}
